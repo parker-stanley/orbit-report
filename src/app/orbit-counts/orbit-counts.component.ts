3e212cab699b50569ed188d071836373bdc145ce
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-09-21 16:18:50 -0400

    almost have the dynamic count working

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -8,9 +8,32 @@ import { Satellite } from '../satellite';
 })
 export class OrbitCountsComponent implements OnInit {
   @Input() satellites: Satellite[];
-  constructor() { }
+  types: string[];
+
+  constructor() {
+    this.types = ['Total', 'Space Debris', 'Communication', 'Probe', 'Positioning', 'Space Station', 'Telescope'];
+   }
 
   ngOnInit() {
   }
+
+  count(satelliteType:string){
+    let count = 0;
+    satelliteType.toLowerCase();
+    if (satelliteType == 'total'){
+      count = this.satellites.length;
+      return count;
+    } else {
+      for (let i = 0; i < this.satellites.length; i++){
+        console.log(this.satellites.length)
+        this.satellites[i].type.toLowerCase();
+        satelliteType.toLowerCase();
+          if(this.satellites[i].type === satelliteType){
+            count++ ; 
+          }
+      }
+    }
+    return count;
+}
 
 }
